6f5f0c50103c1fab9b40fb1875f36f7a5148dbeb
Oliver Consterla <oliver_consterla@yahoo.cl>
2022-01-13 20:27:30 -0300

    Config Api-Rest

---
+++ api/main.py
@@ -0,0 +1,50 @@
+import pymysql
+from app import app
+from config import mysql
+from flask import jsonify
+from flask import flash, request
+
+@app.route('/emp')
+def emp():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT id, name, email, phone, address FROM rest_emp")
+		empRows = cursor.fetchall()
+		respone = jsonify(empRows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/emp/<int:id>')
+def emp(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT id, name, email, phone, address FROM rest_emp WHERE id =%s", id)
+		empRow = cursor.fetchone()
+		respone = jsonify(empRow)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Record not found: ' + request.url,
+    }
+    respone = jsonify(message)
+    respone.status_code = 404
+    return respone
+		
+if __name__ == "__main__":
+    app.run()
