c3e4248f1f893d6fc6b930cf82799d3d46363bd3
Oliver Consterla <oliver_consterla@yahoo.cl>
2022-01-13 22:53:08 -0300

    Eliminacion de codigos erroneos

--- api/app.py
+++ api/app.py
@@ -3,7 +3,3 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
-
-print('Content-type: text/html\r\n\r')
-print("<p>hello world!</p>")
-print("I can view this in my browser yay!!")
