cf51a290fa2907e27e7c744fc729bfaeaabf01d7
Oliver Consterla <oliver_consterla@yahoo.cl>
2022-01-13 22:20:24 -0300

    Cambio lugar de api

--- api/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
-CORS(app)
-
-print('Content-type: text/html\r\n\r')
-print("<p>hello world!</p>")
-print("I can view this in my browser yay!!")
