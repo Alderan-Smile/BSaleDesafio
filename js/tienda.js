55896ec1122c4893b816a3cfed27bc7a7dca3f50
Oliver Consterla <oliver_consterla@yahoo.cl>
2022-01-20 20:57:08 -0300

    Alpha 0.1

--- js/tienda.js
+++ js/tienda.js
@@ -1,7 +1,22 @@
-function putGreeting() {
+var getproduct = 'https://xq17f6g7gc.execute-api.us-east-1.amazonaws.com/dev/product/'
+var getcategory = 'https://xq17f6g7gc.execute-api.us-east-1.amazonaws.com/dev/category/'
+
+$('#buscar').val("").trigger("input")
+
+$('#buscar').on('input', function(){
+    alert("changed");
+});
+
+async function getcontent(val){
+    const response = await fetch(getproduct);
+    const row = await response.json();
+    console.log(row)
+}
+
+/*function putGreeting() {
     var p_hi;
     p_hi = $('#hi');
     p_hi.text('Hello world!');
-}
+}*/
 
-$(document).ready(putGreeting);
+$(document).ready(getcontent());
