f0185aceef18cb63f04ab6266431e59782b025d1
Oliver Consterla <oliver_consterla@yahoo.cl>
2022-01-22 13:36:01 -0300

    Get Change 9

--- js/tienda.js
+++ js/tienda.js
@@ -4,7 +4,9 @@ var myHeaders = new Headers();
 var myInit = {
                 mode: 'cors',
                 headers: {
+                    "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
                     'Access-Control-Allow-Origin': '*',
+                    "Access-Control-Allow-Methods": "OPTIONS,GET"
                 },
                 cache: 'default'
             };
